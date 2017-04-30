4f1325ee18e447204e8005b0d2cf3343aa908652
Nicholas Gilpin <nicholascgilpin@yahoo.com>
2017-04-29 23:51:31 -0500

    Compile script now appends.o and compiles example/intelli-lru

--- compile.sh
+++ compile.sh
@@ -1,4 +1,5 @@
-g++ -Wall --std=c++11 -o lru-config1.o example/lru.cc lib/config1.a
+g++ -Wall --std=c++11 -o intelli-lru-config1.o example/intelli-lru.cc lib/config1.a
+# g++ -Wall --std=c++11 -o lru-config1.o example/lru.cc lib/config1.a
 # g++ -Wall --std=c++11 -o lru-config2.o example/lru.cc lib/config2.a
 # g++ -Wall --std=c++11 -o lru-config3.o example/lru-8MB.cc lib/config3.a
 # g++ -Wall --std=c++11 -o lru-config4.o example/lru-8MB.cc lib/config4.a
